467d2dec075f831870f81490b500b7732b77aa30
Christopher <c.kearsley-brown@edu.salford.ac.uk>
2022-05-02 16:41:15 +0100

    images and test example work

--- search.php
+++ search.php
@@ -12,10 +12,6 @@ $view->pageTitle = "Search";
 global $resultsMessage;
 $resultsMessage = "Here are your results.";
 
-//Creates user
-$array = $_SESSION["user"];
-$user = new User($array[0], $array[1], $array[2], $array[3], $array[4], $array[5], $array[6], $array[7]);
-
 //Used to show the user's friend notifications
 if (isset($_SESSION['loggedIn'])) {
     if ($_SESSION['loggedIn']) {
